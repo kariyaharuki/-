957ec8339c7f0d0f7bca2fc839caf064da014749
kariyaharuki <vantan126@outlook.jp>
2025-07-17 17:18:39 +0900

    Create gameScene.cpp
    GameScene.cppです

---
+++ gameScene.cpp
@@ -0,0 +1,14 @@
+#include "GameScene.h"
+#include "../dxe/dxe.h"
+
+void GameScene::Init() {
+    // ここでゲーム開始時の初期化を書く
+}
+
+void GameScene::Update(float delta_time) {
+    // ゲームロジック（ブロック崩し等のメイン）を書く
+}
+
+void GameScene::Draw() const {
+    // ゲーム画面の描画を書く
+}
